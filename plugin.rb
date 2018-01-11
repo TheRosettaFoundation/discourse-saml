e8b6b5266ee1fd9bea0a1b1fed413e6a068ee2d9
Alan Barrett <alanabarrett0@gmail.com>
2018-01-11 12:30:53 +0000

    Force full screen login

--- plugin.rb
+++ plugin.rb
@@ -99,5 +99,5 @@ auth_provider :title => button_title,
               :frame_width => 600,
               :frame_height => 380,
               :background_color => '#003366',
-              :full_screen_login => GlobalSetting.try(:saml_full_screen_login) || false,
+              :full_screen_login => true,
               :custom_url => request_method == 'post' ? "/discourse_saml" : nil
